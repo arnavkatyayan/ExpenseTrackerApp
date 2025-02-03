c5629ac54aa2faf1facc1f43d9c8f5fe293a084f
Arnav Katyayan <ishumisra99@gmail.com>
2025-02-03 21:27:26 +0530

    Logo and css changes.

--- ExpenseTrackerFE/expensetrackerfe/src/App.js
+++ ExpenseTrackerFE/expensetrackerfe/src/App.js
@@ -3,6 +3,8 @@ import LoginPage from './LoginPage';
 import { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import ChangePasswordPage from './ChangePasswordPage';
+import Logo from './LogoFinal.jpg';
+
 function App() {
   const [isLogoutClicked, setIsLogoutClicked] = useState(false);
   const [currentDateTime, setCurrentDateTime] = useState("");
@@ -34,17 +36,15 @@ function App() {
     return () => clearInterval(interval); // Cleanup interval on component unmount
 }, []);
 
-  // if(isChangePassClicked) {
-  //   <ChangePasswordPage/>
-  // }
 
   return (
     <div>
       <div className='heading'>
-            <h1 className='heading-name'>Expense Tracker</h1>
-            <p className="date-time">{currentDateTime}</p>
+            <h1 className='heading-name'><img src={Logo} className='logo'/> ExpensiFy</h1>
+            {isLogoutClicked ? 
+            <p className="date-time">{currentDateTime}</p>: <p className="date-time login-date">{currentDateTime}</p> }
             {/* {isChangePassClicked ? <Button variant="danger" type="button" className='logout-btn' onClick={handleChangePass}>Change Password</Button>:null} */}
-            <Button variant="danger" type="button" className='logout-btn' onClick={handleChangePass}>{!isChangePassClicked ? "Change Password" : "Go Back"}</Button>
+            {isLogoutClicked ? <Button variant="danger" type="button" className='logout-btn' onClick={handleChangePass}>{!isChangePassClicked ? "Change Password" : "Go Back"}</Button>:null}
             {isLogoutClicked ? <Button variant="danger" type="button" className='logout-btn' onClick={handleRecurringExpense}>Recurring Expense</Button> : null}
             {isLogoutClicked ? <Button variant="danger" type="button" className='logout-btn' onClick={handleLogout}>Logout</Button> : null}
       </div>
@@ -52,7 +52,7 @@ function App() {
       <LoginPage isLogoutClicked={isLogoutClicked} setIsLogoutClicked={setIsLogoutClicked} isChangePassClicked={isChangePassClicked} setIsChangePassClicked={setIsChangePassClicked} isRecurringExpenseClicked={isRecurringExpenseClicked} setIsRecurringExpenseClicked={setIsRecurringExpenseClicked}/>
       {/* <ChangePasswordPage/> */}
       <footer className='footer'>
-        <p>2025.1.3</p>
+        <p>2025.1.4</p>
       </footer>
     </div>
   );
